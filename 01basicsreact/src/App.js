e448b20a2662aacaab0b06951311cf2d61cfefe1
Z-Maniac-Sidd <siddharthghosh78@gmail.com>
2025-12-26 00:54:48 +0530

    after 2nd vid

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>React idk what</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <h1> vite wala </h1>
+  )
+}
+
+export default App
