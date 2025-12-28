f92febb578a97a87d5e02b1943a0d8600a0604f8
Z-Maniac-Sidd <siddharthghosh78@gmail.com>
2025-12-29 02:53:55 +0530

    initiated currency proj, setup and all

---
+++ 06currencyconverter/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-3xl bg-orange-500 text-center font-bold'>currency app</h1>
+    </>
+  )
+}
+
+export default App
