f26af20bf58f9c7fa10a8851bc97bfc09eaa897b
Z-Maniac-Sidd <siddharthghosh78@gmail.com>
2025-12-28 20:58:26 +0530

    added tailwind props and components

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  let myObj = {
+    username : "sidd",
+    age : 21
+  }
+
+  let newarr = [1,2,3,4]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="sidd" btnText = "Click Me"/> 
+      <Card username={ "Anni"}/>
+    </>
+  )
+}
+
+export default App
